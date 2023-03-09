b3be8bdaf78830567d843f743f23d0837a02955d
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2023-03-09 21:30:11 +0300

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -10,10 +10,10 @@ export default function App() {
   return (
     <>
     <div className="page">
-      {/* <Header/> */}
+      <Header/>
       <Switch>
         <Route exact path="/">
-          {/* <Introduction /> */}
+          <Introduction />
           <AboutProject />
           <h1>Route /</h1>
         </Route>
