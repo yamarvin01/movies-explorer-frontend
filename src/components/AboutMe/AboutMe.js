b3921a7cb943f7ba86f916c177068f236182cacb
Marsel Ishmukhametov <yamarvin01@yandex.ru>
2023-03-11 21:19:29 +0300

    changes

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -5,10 +5,12 @@ export default function AboutMe() {
   return (
     <div className="aboutme">
     <h2 className="aboutme__title">СтудентСтудентСтудентСтудентСтудентСтудентСтудентСтудентСтудентСтудентСтудентСтудентСтудентСтудент</h2>
-      <div className="aboume__separator"></div>
-      <div className="aboume__content">
+      <div className="abotme__separator"></div>
+
+
+      <div className="aboutme__content">
         <div className="aboutme__info">
-          <h3 className="aboutme__subtitle">МарсельМарсельМарсельМарсельМарсельМарсельМарсельМарсельМарсельМарсельМарсель</h3>
+          <h3 className="aboutme__subtitle">Марсель Марсель Марсель Марсель Марсель Марсель Марсель Марсель</h3>
           <p className="aboutme__text">Фронтенд-разработчик, 30 летФронтенд-разработчик, 30 летФронтенд-разработчик, 30 летФронтенд-разработчик, 30 летФронтенд-разработчик, 30 лет</p>
           <p className="aboutme__paragraph">
             Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь.
@@ -21,7 +23,18 @@ export default function AboutMe() {
             Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур».
             После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.
           </p>
-        <a className="aboutme__link" href="https://github.com/yamarvin01" target="blank">GitHubGitHubGitHubGitHubGitHubGitHubGitHubGitHubGitHubGitHubGitHubGitHubGitHubGitHubGitHubGitHub</a>
+          <a className="aboutme__link" href="https://github.com/yamarvin01" target="blank">GitHubGitHub GitHubGitHubG itHubGitHubGitHub GitHubGitHubGitH bGitHubGitHubGitH ubGitHubGitH ubGitHub</a>
+        </div>
+
+
+        <div className="aboutme__img-container">
+          <img className="aboutme__img" src={imgMe} alt="Изображение меня"></img>
+        </div>
+        <div className="aboutme__img-container">
+          <img className="aboutme__img" src={imgMe} alt="Изображение меня"></img>
+        </div>
+        <div className="aboutme__img-container">
+          <img className="aboutme__img" src={imgMe} alt="Изображение меня"></img>
         </div>
         <div className="aboutme__img-container">
           <img className="aboutme__img" src={imgMe} alt="Изображение меня"></img>
